ccaa42719ae6f0e8065759f95a65a775a68f00b2
Mingela <freekilluau@gmail.com>
2019-05-14 10:03:45 +0300

    Added copyright

--- dapp-core/src/test/resources/sample_contract.groovy
+++ dapp-core/src/test/resources/sample_contract.groovy
@@ -1,3 +1,8 @@
+/*
+ * Copyright D3 Ledger, Inc. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 import io.reactivex.Observable
 import io.reactivex.disposables.Disposable
 import iroha.protocol.Commands
