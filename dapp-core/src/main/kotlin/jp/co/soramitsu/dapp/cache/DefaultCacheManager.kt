856fe2f17ea85bdd6265ac9c3764546363e3460e
Mingela <freekilluau@gmail.com>
2019-05-15 10:22:28 +0300

    Strict string cache

--- dapp-core/src/main/kotlin/jp/co/soramitsu/dapp/cache/DefaultCacheManager.kt
+++ dapp-core/src/main/kotlin/jp/co/soramitsu/dapp/cache/DefaultCacheManager.kt
@@ -14,32 +14,22 @@ class DefaultCacheManager : CacheManager {
     private val cache = HashMap<String, Any?>()
 
     @Synchronized
-    override fun put(key: String?, value: Any?): Any? {
-        checkArgument(key)
-        return cache.put(key!!, value)
+    override fun put(key: String, value: Any?): Any? {
+        return cache.put(key, value)
     }
 
     @Synchronized
-    override fun get(key: String?): Any? {
-        checkArgument(key)
+    override fun get(key: String): Any? {
         return cache[key]
     }
 
     @Synchronized
-    override fun remove(key: String?): Any? {
-        checkArgument(key)
+    override fun remove(key: String): Any? {
         return cache.remove(key)
     }
 
     @Synchronized
-    override fun contains(key: String?): Boolean {
-        checkArgument(key)
+    override fun contains(key: String): Boolean {
         return cache.contains(key)
-    }
-
-    private fun checkArgument(key: String?) {
-        if (key == null) {
-            throw IllegalArgumentException("Null keys are not allowed")
-        }
     }
 }
