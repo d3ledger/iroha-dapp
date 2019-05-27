b10ceb2204d086ad6da034d0d2cc13b8b52af58b
Bogdan Mingela <freekilluau@gmail.com>
2019-05-27 10:17:03 +0300

    Demo test for exchanger (#6)

--- dapp-core/src/main/kotlin/jp/co/soramitsu/dapp/config/DappContextConfiguration.kt
+++ dapp-core/src/main/kotlin/jp/co/soramitsu/dapp/config/DappContextConfiguration.kt
@@ -8,10 +8,10 @@ package jp.co.soramitsu.dapp.config
 import com.d3.commons.config.RMQConfig
 import com.d3.commons.config.loadRawLocalConfigs
 import com.d3.commons.sidechain.iroha.ReliableIrohaChainListener
-import com.d3.commons.sidechain.iroha.util.ModelUtil.loadKeypair
 import com.d3.commons.util.createPrettySingleThreadPool
 import jp.co.soramitsu.iroha.java.IrohaAPI
 import jp.co.soramitsu.iroha.java.QueryAPI
+import jp.co.soramitsu.iroha.java.Utils
 import mu.KLogging
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -24,7 +24,10 @@ class DappContextConfiguration {
     private val rmqConfig = loadRawLocalConfigs(DAPP_NAME, RMQConfig::class.java, "rmq.properties")
 
     @Bean
-    fun dappKeyPair() = loadKeypair(dappConfig.pubKeyPath, dappConfig.privKeyPath).get()
+    fun dappKeyPair() = Utils.parseHexKeypair(
+        dappConfig.pubKey,
+        dappConfig.privKey
+    )
 
     @Bean
     fun irohaApi() = IrohaAPI(URI(dappConfig.irohaUrl))
